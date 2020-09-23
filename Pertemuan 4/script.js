8fd3f35a66a08df81a4bac24cf26fad167377ab1
jourdiee99 <70786679+jourdiee99@users.noreply.github.com>
2020-09-23 23:53:26 +0700

    Add files via upload

---
+++ Pertemuan 4/script.js
@@ -0,0 +1,7 @@
+function changeLine1Style() {
+    document.getElementById("cLine1").className = "makeRed";
+}
+
+function changeStyle(id, classname) {
+    document.getElementById(id).className = classname;
+}
